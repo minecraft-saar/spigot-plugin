16f21869f7139af38fe34a8e4944dc5711cf4f69
ca-schaefer <cschaef@coli.uni-saarland.de>
2019-10-18 11:16:30 +0200

    One spare preloaded Player world, player teleports to new world when joining

--- worldtest/src/main/java/de/saar/minecraft/worldtest/FlatChunkGenerator.java
+++ worldtest/src/main/java/de/saar/minecraft/worldtest/FlatChunkGenerator.java
@@ -9,52 +9,57 @@ import java.util.Random;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.generator.ChunkGenerator;
+import org.bukkit.util.noise.SimplexOctaveGenerator;
 
 public class FlatChunkGenerator extends ChunkGenerator {
+    //int currentHeight = 10;
 
-  public byte[][] generateBlockSections(World world, Random random, int x, int z, BiomeGrid biomeGrid)
-  {
-    byte[][] result = new byte[world.getMaxHeight() / 16][]; //world height / chunk part height (=16, look above)
+    @Override
+    public ChunkData generateChunkData(World world, Random random, int chunkX, int chunkZ, BiomeGrid biome) {
+        // TODO Chunk generation code here.
+        //SimplexOctaveGenerator generator = new SimplexOctaveGenerator(new Random(world.getSeed()), 8);
+        ChunkData chunk = createChunkData(world);
+        //generator.setScale(0.005D);
 
-     // Set ground layer of bedrock
-    for(x = 0; x < 16; x++)
-    {
-      for(z = 0; z < 16; z++)
-      {
-        setBlock(result, x, 0, z, (byte) Material.BEDROCK.getId());
-      }
-    }
-  // Grass layer on top
-    for(x = 0; x < 16; x++)
-    {
-      for(z = 0; z < 16; z++)
-      {
-        setBlock(result, x, 1, z, (byte)Material.GRASS.getId());
-      }
+        for (int x = 0; x < 16; x++)
+            for (int z = 0; z < 16; z++) {
+                chunk.setBlock(x, 1, z, Material.GRASS);
+                chunk.setBlock(x, 0, z, Material.BEDROCK);
+            }
+        return chunk;
     }
 
-    return result;
-  }
+    public byte[][] generateBlockSections(World world, Random random, int x, int z, BiomeGrid biomeGrid) {
+        byte[][] result = new byte[world.getMaxHeight() / 16][]; //world height / chunk part height (=16, look above)
 
-  void setBlock(byte[][] result, int x, int y, int z, byte blkid) {
-    // is this chunk part already initialized?
-    if (result[y >> 4] == null) {
-      // Initialize the chunk part
-      result[y >> 4] = new byte[4096];
+        // Set ground layer of bedrock
+        for (x = 0; x < 16; x++) {
+            for (z = 0; z < 16; z++) {
+                setBlock(result, x, 0, z, (byte) Material.BEDROCK.getId());
+            }
+        }
+        // Grass layer on top
+        for (x = 0; x < 16; x++) {
+            for (z = 0; z < 16; z++) {
+                setBlock(result, x, 1, z, (byte) Material.GRASS.getId());
+            }
+        }
+        return result;
     }
-    // set the block (look above, how this is done)
-    result[y >> 4][((y & 0xF) << 8) | (z << 4) | x] = blkid;
-  }
+
+    private void setBlock(byte[][] result, int x, int y, int z, byte blkid) {
+        // is this chunk part already initialized?
+        if (result[y >> 4] == null) {
+            // Initialize the chunk part
+            result[y >> 4] = new byte[4096];
+        }
+        // set the block (look above, how this is done)
+        result[y >> 4][((y & 0xF) << 8) | (z << 4) | x] = blkid;
+    }
 
 //  @Override
 //  public Location getFixedSpawnLocation​(World world, Random random) {
 //    return new Location(world, 0, 0, 0);
 //  }
 
-//  private void setBlock(byte[][] result, int x, int y, int z, byte blockId) {
-//    if (result[y >> 4] == null) {
-//      result[y >> 4] = new byte[4096];
-//    }
-//    result[y >> 4][((y & 0xF) << 8) | (z << 4) | x] = blockId;
-//  }
 }
--- worldtest/src/main/java/de/saar/minecraft/worldtest/MinecraftListener.java
+++ worldtest/src/main/java/de/saar/minecraft/worldtest/MinecraftListener.java
@@ -15,15 +15,22 @@ import org.bukkit.event.weather.WeatherChangeEvent;
 import org.bukkit.event.world.StructureGrowEvent;
 import org.bukkit.event.world.WorldInitEvent;
 
+import java.util.HashMap;
+
 
 public class MinecraftListener implements Listener {
     WorldCreator creator;
+    World nextWorld;  // Preloaded world for the next joining player
+
+    HashMap<String, World> activeWorlds = new HashMap<String, World>();
 
     public MinecraftListener(){
         super();
 
-        creator = new WorldCreator("base");
+        creator = new WorldCreator("playerworld0");
         creator.generator(new FlatChunkGenerator());
+        nextWorld = creator.createWorld();
+
 
     }
 
@@ -31,14 +38,21 @@ public class MinecraftListener implements Listener {
     public void onPlayerJoin(PlayerJoinEvent event) {
         String playerName = event.getPlayer().getDisplayName();
         System.out.println("First world " + event.getPlayer().getWorld().getName());
-        // TODO: create new world
-        //World baseWorld = event.getPlayer().getWorld();
-        World currentWorld = creator.createWorld();
-        Location teleportLocation = currentWorld.getSpawnLocation();
+
+        Location teleportLocation = nextWorld.getSpawnLocation();
         boolean worked = event.getPlayer().teleport(teleportLocation);
         System.out.format("Teleportation worked %b", worked);
         System.out.println("Second world " + event.getPlayer().getWorld().getName());
         Bukkit.broadcastMessage("Welcome to the server, " + playerName);
+
+        // Add world to active worlds
+        activeWorlds.put(nextWorld.getName(), nextWorld);
+
+        // Create new preloaded world for the next player
+        String worldName = "playerworld" + activeWorlds.size();
+        creator = new WorldCreator(worldName);
+        creator.generator(new FlatChunkGenerator());
+        nextWorld = creator.createWorld();
     }
 
     @EventHandler
